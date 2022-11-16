f14390385dec723206ce4a896cf60f9e843aafe5
TheSkibb <kfskibrek@icloud.com>
2022-11-16 10:06:32 +0100

    modified sql files

--- project/sqlFiles/createTables.sql
+++ project/sqlFiles/createTables.sql
@@ -1,22 +1,38 @@
 use project
 
+create or replace table kjonn(
+  id int not null primary key,
+  kjonn varchar(255)
+);
+
+create or replace table brukerType(
+  id int not null primary key,
+  brukerType varchar(255) not null
+);
+
+create or replace table boligtype(
+  id int not null primary key,
+  boligtype varchar(255) not null
+);
 
 create or replace table bruker(
-  id int not null, 
+  id int not null primary key, 
   fornavn varchar(255) not null,
   etternavn varchar(255) not null,
   epost varchar(254) not null,
-  type int,
-  kjonn int,
+  brukerTypeId int, 
+  kjonnId int,
   beskrivelse varchar(500),
-  passord varchar(255)
+  passord varchar(255),
+  FOREIGN KEY(kjonnId) REFERENCES kjonn(id),
+  FOREIGN KEY(brukerTypeId) REFERENCES brukerType(id)
 );
 
 create or replace table annonser(
-  id int not null,
+  id int not null primary key,
   eier int not null,
   gate varchar(255) not null,
-  postnummer int not null,
+  postnummer int not null, 
   leie int not null,
   depositum int not null,
   beskrivelse varchar(255),
@@ -29,13 +45,37 @@ create or replace table annonser(
   internettInkl boolean,
   tvInkl boolean,
   moblert boolean,
-  boligtype int,
+  boligtype int, 
   soveromAnt int,
   badAnt int,
   kvadrat int
 );
 
 create or replace table favoritter(
-  brukerID int not null,
-  annonseID int not null
+  brukerId int not null, 
+  annonseId int not null, 
+  primary key(brukerId, annonseId)
 );
+
+create or replace table bilde(
+  id int not null primary key,
+  bildeLenke varchar(255) not null
+);
+
+create or replace table bildeAnnonser(
+  annonseId int not null, 
+  bildeId int not null,
+  FOREIGN KEY(annonseId) REFERENCES annonser(id),
+  FOREIGN KEY(bildeId) REFERENCES bilde(id)
+);
+
+create or replace table postnummer(
+  id int not null primary key,
+  poststedId int not null 
+);
+
+create or replace table poststed(
+  id int not null primary key,
+  poststed varchar(255)
+);
+
--- project/sqlFiles/runScripts.sh
+++ project/sqlFiles/runScripts.sh
@@ -1,3 +1,5 @@
 echo 'creating tables..'
+{ echo test ; } | mysql -h 172.17.0.2 -u root -p < dropTables.sql
 mysql -h 172.17.0.2 -u root -p < createTables.sql
+echo 'test'
 echo 'tables created'
