c0112aefc5f7b72baf9d6f3617f8fa4d75c5e895
TheSkibb <kfskibrek@icloud.com>
2022-11-20 17:26:25 +0100

    change from constants to variables to avoid collisions when importing this library multiple times

--- project/htdocs/lib/database.inc.php
+++ project/htdocs/lib/database.inc.php
@@ -5,15 +5,15 @@ error_reporting(E_ALL);
 ?>
 
 <?php
-define("DB_VERT", "172.17.0.2");
-define("DB_BRUKER", "php");
-define("DB_PASS", "123");
-define("DB_NAVN", "project");
+$DB_VERT = "172.17.0.2";
+$DB_BRUKER = "php";
+$DB_PASS = "123";
+$DB_NAVN = "project";
 
-$dkn = "mysql:host=" . DB_VERT . "; dbname=" . DB_NAVN;
+$dkn = "mysql:host=" . $DB_VERT . "; dbname=" . $DB_NAVN;
 
 try{
-  $pdo = new PDO($dkn, DB_BRUKER, DB_PASS);
+  $pdo = new PDO($dkn, $DB_BRUKER, $DB_PASS);
 }
 catch(PDOException $e){
   echo "feil ved tilkobling til db
