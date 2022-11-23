800c98c1e515692d0a4252d34a1bc02071ecb41e
TheSkibb <kfskibrek@icloud.com>
2022-11-23 10:41:53 +0100

    moved navigation bar to separate file

--- project/htdocs/assets/favoritt.php
+++ project/htdocs/assets/favoritt.php
@@ -11,25 +11,7 @@
 </head>
 <body>
 <!--Navigasjonsbar  -->
-<div class="topnav">
-  <a class="active" href="hjem.php">Hjem</a>
-  <a href="profil.php">Profil</a>
-  <a href="favoritt.php">♥</a>
-  <a href="logginn.php">Logg inn</a>
-  <div class="sokeknapp">
-    <form action="sok.php">
-      <input type="text" placeholder="Søk.." name="search">
-      <button type="submit"><i class="fa fa-search"></i></button>
-    </form>
-  </div>
-</div>
-
-
-<!--hoved siden -->
-<div class="hoved">
-<h2>Favoritter</h2>
-  
-  </div>
+<?php require_once("./../lib/navbar.php");?>
 
 
 
