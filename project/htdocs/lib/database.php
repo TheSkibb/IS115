af24b3546309405369f6428d664ddba780a569d1
TheSkibb <kfskibrek@icloud.com>
2022-11-30 11:51:40 +0100

    get user from database

--- project/htdocs/lib/database.php
+++ project/htdocs/lib/database.php
@@ -5,11 +5,11 @@
 #$db_navn = "prosjekt"; 
 
 #$con = mysqli_connect($host, $bruker, $passord, $db_navn);
+$mysql_hostname = "172.17.0.2";
+$mysql_user = "php";
+$mysql_password = "123";
+$mysql_database = "project";
 
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
-$mysql_database = "prosjekt";
 $con = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database);
 
 #$con = mysqli_connect("localhost","root","","prosjekt");
