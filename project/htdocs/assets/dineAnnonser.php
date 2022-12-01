6c537e827630f8f050cf4504bccfe776030be2eb
TheSkibb <kfskibrek@icloud.com>
2022-12-01 11:59:14 +0100

    dine annonser page can now delete annonser

--- project/htdocs/assets/dineAnnonser.php
+++ project/htdocs/assets/dineAnnonser.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Page Title</title>
+    <title>Dine annonser</title>
+    <link rel="stylesheet" href="../static/stylesNavBar.css" />
   </head>
   <body>
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+require_once("./../lib/navbar.php");
+
+//TODO: use actual user id
+$bruker = 1;
+
+//filter for annonsene som skal vises
+$searchFilter = ' where eier = ' . $bruker;
+
+//hent alle annonser med filteret, eier er satt til true
+include "./../lib/classes/hjemAnnonse.php";
+HjemAnnonse::getAllAnnonser($searchFilter, true);
 ?>
+
   </body>
 </html>
---
+++ project/htdocs/assets/redigerAnnonse.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Rediger Annonse</title>
+    <link rel="stylesheet" href="./../static/stylesNavBar.css">
+  </head>
+  <body>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once('./../lib/navbar.php');
+
+//TODO: use actual brukerid + check if user = owner
+$bruker = 1;
+
+?>
+  </body>
+</html>
