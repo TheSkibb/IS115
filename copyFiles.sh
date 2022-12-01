52f5abe0356e2d0cf24a926e6d637b4c15fac27b
TheSkibb <kfskibrek@icloud.com>
2022-12-01 09:39:45 +0100

    change owner of images, so images can be uploaded

--- copyFiles.sh
+++ copyFiles.sh
@@ -1,6 +1,8 @@
 echo 'copying files..'
 
 sudo cp -r ./project $PHP/
+sudo chown daemon:daemon /opt/lampp/htdocs/project/htdocs/images
+sudo chown daemon:daemon /opt/lampp/htdocs/project/htdocs/images/*
 
 echo 'files copied'
 
