2086a10300c77c7619677e1367ef95966a26fbf5
TheSkibb <kfskibrek@icloud.com>
2022-12-01 12:39:40 +0100

    add dine meldinger;

--- project/htdocs/assets/dineMeldinger.php
+++ project/htdocs/assets/dineMeldinger.php
@@ -2,12 +2,16 @@
 <html>
   <head>
     <title>Page Title</title>
+    <link rel="stylesheet" href="./../static/stylesNavBar.css">
   </head>
   <body>
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+require_once('./../lib/navbar.php');
+//TODO: add actual user
+$bruker = 1;
 ?>
   </body>
 </html>
