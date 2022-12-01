66ddf02737e3f2e07229005baac3766029b0e59b
TheSkibb <kfskibrek@icloud.com>
2022-12-01 09:44:10 +0100

    add dinemeldinger page

---
+++ project/htdocs/assets/dineMeldinger.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Page Title</title>
+  </head>
+  <body>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+  </body>
+</html>
