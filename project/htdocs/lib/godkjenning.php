679581eb1dfbbba1e26ad603da162a981f1db3c5
Julia Ottesen <juliaottesen@Brukers-MacBook-Pro.local>
2022-12-02 11:58:10 +0100

    add session variables

--- project/htdocs/lib/godkjenning.php
+++ project/htdocs/lib/godkjenning.php
@@ -6,13 +6,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 	$brukernavn=mysqli_real_escape_string($con,$_POST['brukernavn']);
 	$passord=mysqli_real_escape_string($con,$_POST['passord']);
 	
-	$sql="SELECT passord FROM bruker WHERE brukernavn='$brukernavn' LIMIT 1";
+	$sql="SELECT passord, id FROM bruker WHERE brukernavn='$brukernavn' LIMIT 1";
 	$result=mysqli_query($con,$sql);
 	$row=mysqli_fetch_array($result);
 	#$active=$row['active'];
 	$count=mysqli_num_rows($result);
 	$passord_hash = $row['passord'];
 	if (password_verify($passord, $passord_hash)) {
+		$_SESSION['brukernavn']=$brukernavn;
+		$_SESSION['userId']=$row["id"];
 		header("location: ../assets/profil.php");
 	} else {
 		header('location: ../assets/logginn.php?remarks=failed');
